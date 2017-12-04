40154ace092759e432f44f21c9c1d5c5df6063c4
Josh <joshsweitzer@gmail.com>
2017-12-04 18:06:48 -0500

    init commit

---
+++ DailyProgrammer/DailyProgrammer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DailyProgrammer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
