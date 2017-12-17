605709061cf12fe8b0b8e84bb5a354ce4a923835
Josh <joshsweitzer@gmail.com>
2017-12-17 14:49:55 -0500

    Added vanilla socket http get request from MS docs

--- DailyProgrammer/DailyProgrammer/Program.cs
+++ DailyProgrammer/DailyProgrammer/Program.cs
@@ -18,6 +18,7 @@ namespace DailyProgrammer
                     case "scales": MajorScales.Go(); break;
                     case "ASCII": ASCII85Utility.Go(); break;
                     case "Polynomials": PolynomialDivision.Go(); break;
+                    case "WebClient": WebClient.SocketSendReceive("www.stealmylogin.com", 80); break;
                 }
             }
         }
