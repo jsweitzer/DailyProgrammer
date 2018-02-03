c740a55d8d97372b59cada5faa1476a16a5cbbc4
Josh <joshsweitzer@gmail.com>
2018-02-03 09:26:40 -0500

    starting stacking

---
+++ DailyProgrammer/DailyProgrammer/Solutions/StackingBoxes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyProgrammer.Solutions
+{
+    //https://www.reddit.com/r/dailyprogrammer/comments/7ubc70/20180130_challenge_349_intermediate_packing/
+    public class StackingBoxes
+    {
+
+    }
+}
