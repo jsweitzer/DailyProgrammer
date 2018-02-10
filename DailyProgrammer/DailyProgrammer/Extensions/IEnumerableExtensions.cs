666fbe922a497520c39eb8dcc7739584c791d67f
Josh <joshsweitzer@gmail.com>
2018-02-10 10:55:25 -0500

    Revert "krak"
    This reverts commit 0ea7a93f05992c48e7436cd6d3d8e319b1811bf7.

---
+++ DailyProgrammer/DailyProgrammer/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DailyProgrammer.Extensions
+{
+    public static class IEnumerableExtensions
+    {
+        public static IEnumerable<U> Map<T, U>(this IEnumerable<T> s, Func<T, U> f)
+        {
+            foreach (var i in s)
+                yield return f(i);
+        }
+    }
+}
